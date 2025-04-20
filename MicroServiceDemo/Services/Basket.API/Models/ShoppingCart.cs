33df2c892bab72932894e8aaea5ee08041b8faad
Foysal <ahfcse@gmail.com>
2025-04-20 23:41:20 +0600

    basketAPI

---
+++ MicroServiceDemo/Services/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        public ShoppingCart()
+        {
+                
+        }
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public decimal TotalPrice
+        {
+            get {
+                decimal total = 0;
+                foreach (var item in Items)
+                {
+                    total += item.Price * item.Quantity;
+                }
+                return total;
+            }
+        }
+
+    }
+}
