b113aae4ef2808ac2e4993295977cdea03cd344a
Foysal <ahfcse@gmail.com>
2025-05-05 23:20:24 +0600

    Ordering API Folder steucture add

---
+++ MicroServiceDemo/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public class EntityBase
+    {
+        public int AddedBy { get; set; }
+        public DateTime AddedAt { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
