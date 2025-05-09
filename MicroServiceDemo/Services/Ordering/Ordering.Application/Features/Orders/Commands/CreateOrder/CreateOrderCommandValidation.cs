da09f10d68cae9ff41e22af65fd2a3edd22cea66
Foysal <ahfcse@gmail.com>
2025-05-10 00:28:59 +0600

    Fluent Validation

---
+++ MicroServiceDemo/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CreateOrder
+{
+    public class CreateOrderCommandValidation:AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidation()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty().WithMessage("{UserName} is required.")
+               .EmailAddress().WithMessage("{UserName} is not a valid email address.")
+                .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters.");
+            RuleFor(x => x.Total)
+                .NotEmpty().WithMessage("{TotalPrice} is required.")
+                .GreaterThan(0).WithMessage("{TotalPrice} must be greater than 0.");
+
+        }
+    }
+    
+}
