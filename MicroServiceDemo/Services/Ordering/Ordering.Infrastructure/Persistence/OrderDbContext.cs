835cb591d424eca6fd065f65643782274e85484a
Foysal <ahfcse@gmail.com>
2025-05-30 21:02:33 +0600

    DbContext

---
+++ MicroServiceDemo/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderDbContext:DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Order> Orders { get; set; }
+
+
+
+    }
+}
